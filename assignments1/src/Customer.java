2cb2d49065a01bbd36baee103a39b17b0cd181df
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-02 02:58:29 +0300

    Add setters and getters

--- assignments1/src/Customer.java
+++ assignments1/src/Customer.java
@@ -1,2 +1,29 @@
 public class Customer {
+    private int id;
+    private long startTime;
+    private long endTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
 }

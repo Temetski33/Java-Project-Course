1161f781684bcaf5e3dd8f67c4239388d2350742
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-02 04:47:53 +0300

    Add functional time spent function

--- assignments1/src/Task1_4.java
+++ assignments1/src/Task1_4.java
@@ -3,35 +3,48 @@ import java.util.Scanner;
 
 public class Task1_4 {
     public static void main(String[] args) {
-        LinkedList<Object> queue = new LinkedList<>();
+        LinkedList<Customer> queue = new LinkedList<>();
 
         Customer customer = new Customer();
 
-        System.out.println("Welcome to queue simulator. \n");
+        System.out.println("Welcome to queue simulator.");
+        System.out.println();
         System.out.println("Input action \"queue\", \"dequeue\" or \"exit\": ");
         Scanner reader = new Scanner(System.in);
         String input = reader.nextLine();
+        System.out.println();
         
 
         while (!input.equals("exit")) {
             if (input.equals("queue")) {
-                
+
+                // add customer to LinkedList
                 queue.addFirst(customer);
+                // set start time for the customer
+                queue.getFirst().setStartTime(System.currentTimeMillis());
+                // create a new customer who would be the next to get added to queue
                 customer = new Customer();
 
                 System.out.println("Input action \"queue\", \"dequeue\" or \"exit\": ");
                 input = reader.nextLine();
+                System.out.println();
 
             } else if (input.equals("dequeue")) {
+                // set end time for the customer about to be removed
+                queue.getLast().setEndTime(System.currentTimeMillis());
+                // count time spent
+                System.out.println("Customer " + queue.getLast().getId() + " Id time spent: " + queue.getLast().getTimeSpent() + " milliseconds.");
 
                 queue.removeLast();
 
                 System.out.println("Input action \"queue\", \"dequeue\" or \"exit\": ");
                 input = reader.nextLine();
+                System.out.println();
+            }
+            else {
+                System.out.println("Error! Invalid input.");
             }
         }
-
-        System.out.println(queue);
 
 
     }

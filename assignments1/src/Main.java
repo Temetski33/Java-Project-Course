de5959337dadc08c7caf1a0b1fdacb333babd54e
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-02 03:24:01 +0300

    Create test program

--- assignments1/src/Main.java
+++ assignments1/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer();
+
+        System.out.println("Id is " + customer.getId());
+
+        customer.setStartTime(System.currentTimeMillis());
+        customer.setEndTime(System.currentTimeMillis());
+
+        System.out.println(customer.getStartTime());
+        System.out.println(customer.getTimeSpent());
+
+        Customer customer2 = new Customer();
+        System.out.println("Id is " +customer2.getId());
     }
 }

c79e93bb9066a2727588f4b258d0d7ddaeedbd11
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-08 21:11:23 +0300

    Edit compare method

--- assignments2/src/Event.java
+++ assignments2/src/Event.java
@@ -1,8 +1,23 @@
 public class Event implements Comparable<Event> {
+    private String key;
+    private int value;
 
+    public Event(String key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public int getValue() {
+        return value;
+    }
 
     @Override
     public int compareTo(Event other) {
-        return this.getValue().compareTo(other.getValue());
+        return Integer.compare(this.getValue(), other.getValue());
     }
+
 }

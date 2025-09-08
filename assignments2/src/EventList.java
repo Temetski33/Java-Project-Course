e296547745b65eca198f85d50e1f203ef29816f7
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-08 21:26:29 +0300

    Add initial structure

--- assignments2/src/EventList.java
+++ assignments2/src/EventList.java
@@ -1,2 +1,9 @@
+import java.util.PriorityQueue;
+
 public class EventList {
+    public static void main(String[] args) {
+        PriorityQueue<Event> events = new PriorityQueue<>();
+
+        events.add(new Event("A1", 6));
+    }
 }

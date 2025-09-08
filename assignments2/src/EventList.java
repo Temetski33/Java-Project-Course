da5a319018b12d2ce4c18eff66e7ccea930c11bd
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-08 21:36:33 +0300

    Add while loop to iterate through list and print and remove in order

--- assignments2/src/EventList.java
+++ assignments2/src/EventList.java
@@ -5,5 +5,15 @@ public class EventList {
         PriorityQueue<Event> events = new PriorityQueue<>();
 
         events.add(new Event("A1", 6));
+        events.add(new Event("A2", 9));
+        events.add(new Event("A3", 7));
+        events.add(new Event("A4", 4));
+
+        System.out.println(events);
+
+        while (!events.isEmpty()) {
+            Event firstInLine = events.poll();
+            System.out.println(firstInLine.getKey() + " " + firstInLine.getValue());
+        }
     }
 }

75e4122a8c39b51d78e8fe1b5670811203331e79
Temetski33 <teemu.laasio@metropolia.fi>
2025-09-09 01:09:20 +0300

    Edit prints and inputs

--- assignments2/src/EventList.java
+++ assignments2/src/EventList.java
@@ -5,13 +5,13 @@ public class EventList {
     public static void main(String[] args) {
         PriorityQueue<Event> events = new PriorityQueue<>();
 
-        events.add(new Event("A1", 6));
-        events.add(new Event("A2", 9));
-        events.add(new Event("A3", 7));
-        events.add(new Event("A4", 4));
+        events.add(new Event("ARRIVAL", 6));
+        events.add(new Event("EXIT", 9));
+        events.add(new Event("ARRIVAL", 7));
+        events.add(new Event("EXIT", 4));
 
         Event nextEvent = events.poll();
-        System.out.println("Removing event " + nextEvent.getKey() + " from the list.");
+        System.out.println("Removing event " + nextEvent.getKey() + " from the list (time: " + nextEvent.getValue() + ").");
 
         System.out.println();
         System.out.println("Printing event list:");
@@ -19,7 +19,7 @@ public class EventList {
         Event[] eventArray = events.toArray(new Event[events.size()]);
         Arrays.sort(eventArray, events.comparator());
         for (Event e : eventArray) {
-            System.out.println(e.getKey() + " " + e.getValue());
+            System.out.println(e.getKey() + ", time: " + e.getValue());
         }
     }
 }
